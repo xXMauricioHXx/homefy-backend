8f1a049b1ddccab834459404c920f110eb985c96
Mauricio dos Santos Henrique <mauriciosh558@gmail.com>
2026-03-03 21:32:15 -0300

    feat: enable cord

--- functions/v1/domains/properties/entrypoints/properties.http.ts
+++ functions/v1/domains/properties/entrypoints/properties.http.ts
@@ -6,6 +6,7 @@ import { handleError } from "../../../shared/ErrorHandle";
 import { HttpClient } from "../../../core/infrastructure/http/HttpClient";
 import { StorageClient } from "../../../core/infrastructure/storage/StorageClient";
 import { DewatermarkHttp } from "../../../core/infrastructure/http/DewatermarkHttp";
+import { coreContainer } from "../../../core/container/container";
 
 const container = new PropertiesContainer(
   FirestoreClient.getInstance(),
@@ -14,144 +15,162 @@ const container = new PropertiesContainer(
   DewatermarkHttp.getInstance(),
 );
 
-const REGION = { region: "southamerica-east1" };
+export const listProperties = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "GET") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const listProperties = onRequest(REGION, async (req, res) => {
-  if (req.method !== "GET") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
+    const { uid } = (req as any).user;
 
-  const { uid } = (req as any).user;
+    try {
+      const list = await container.listPropertiesUseCase.execute(uid);
+      res.status(200).json(list);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
 
-  try {
-    const list = await container.listPropertiesUseCase.execute(uid);
-    res.status(200).json(list);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+export const getProperty = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "GET") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const getProperty = onRequest(REGION, async (req, res) => {
-  if (req.method !== "GET") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
+    const id = req.params[0];
 
-  const id = req.params[0];
+    try {
+      const property = await container.getPropertyUseCase.execute(id);
+      res.status(200).json(property);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
 
-  try {
-    const property = await container.getPropertyUseCase.execute(id);
-    res.status(200).json(property);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+export const createProperty = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const createProperty = onRequest(REGION, async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
+    const { uid } = (req as any).user;
 
-  const { uid } = (req as any).user;
+    try {
+      const property = await container.createPropertyUseCase.execute(
+        uid,
+        req.body,
+      );
+      res.status(201).json(property);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
 
-  try {
-    const property = await container.createPropertyUseCase.execute(
-      uid,
-      req.body,
-    );
-    res.status(201).json(property);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+export const updateProperty = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "PUT") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const updateProperty = onRequest(REGION, async (req, res) => {
-  if (req.method !== "PUT") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
+    const id = req.params[0];
+    try {
+      await container.updatePropertyUseCase.execute(id, req.body);
+      res.status(204).send();
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
 
-  const id = req.params[0];
-  try {
-    await container.updatePropertyUseCase.execute(id, req.body);
-    res.status(204).send();
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+export const scrapProperty = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const scrapProperty = onRequest(REGION, async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
+    try {
+      const property = await container.scrapPropertyUseCase.execute(
+        req.body.url,
+      );
+      res.status(200).json(property);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
 
-  try {
-    const property = await container.scrapPropertyUseCase.execute(req.body.url);
-    res.status(200).json(property);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+export const createPdfRendition = onRequest(
+  coreContainer.cloudFunction.defaultConfig(),
+  async (req, res) => {
+    if (req.method !== "POST") {
+      res.status(405).json({ error: "Method not allowed" });
+      return;
+    }
 
-export const createPdfRendition = onRequest(REGION, async (req, res) => {
-  if (req.method !== "POST") {
-    res.status(405).json({ error: "Method not allowed" });
-    return;
-  }
+    try {
+      await AuthMiddleware.verifyToken(req as any, res as any);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+      return;
+    }
 
-  try {
-    await AuthMiddleware.verifyToken(req as any, res as any);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-    return;
-  }
-
-  const propertyId = req.params[0];
-  try {
-    const rendition = await container.createPdfRenditionUseCase.execute(
-      propertyId,
-      req.body.config,
-    );
-    res.status(201).json(rendition);
-  } catch (error) {
-    handleError(res, error, "properties.http");
-  }
-});
+    const propertyId = req.params[0];
+    try {
+      const rendition = await container.createPdfRenditionUseCase.execute(
+        propertyId,
+        req.body.config,
+      );
+      res.status(201).json(rendition);
+    } catch (error) {
+      handleError(res, error, "properties.http");
+    }
+  },
+);
